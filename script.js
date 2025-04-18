deb299f75f5ca52f92e9a4fc96d3c675eaa93011
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-04-18 13:43:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="styles.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
